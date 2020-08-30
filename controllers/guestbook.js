8760afd07d5d23f31148b971f65ba80261701dfc
Stavon Ewart <stavon@Stavons-MacBook-Air.local>
2020-08-30 14:06:23 -0400

    Fixes styling for guestbook

--- controllers/guestbook.js
+++ controllers/guestbook.js
@@ -1,7 +1,25 @@
 const Message = require('../models/message')
 
 module.exports = {
-  index
+  index,
+  create,
+  show,
+}
+
+function show(req, res){
+  Message.findById(req.params.id)
+  .then((message)=>{
+    res.render('guestbook/')
+  })
+}
+
+function create(req, res){
+  req.body.postedBy = req.user.name
+  req.body.avatar = req.user.avatar
+  Message.create(req.body)
+  .then(()=>{
+    res.redirect('/guestbook')
+  })
 }
 
 function index(req, res){
--- routes/guestbook.js
+++ routes/guestbook.js
@@ -1,7 +1,9 @@
 const router = require('express').Router()
 const guestbookCtrl = require('../controllers/guestbook')
 
-router.get('/', isLoggedIn, guestbookCtrl.index)
+router.get('/', isLoggedIn, guestbookCtrl.index);
+router.post('/', isLoggedIn, guestbookCtrl.create);
+// router.get('/:id', isLoggedIn, guestbookCtrl.show);
 
 function isLoggedIn(req, res, next){
   if(req.isAuthenticated()) return next();
