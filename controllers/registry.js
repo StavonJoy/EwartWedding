36f7f8d4ba00c6aa4d1cfa87e4e27f8f97005f70
Stavon Ewart <stavon@Stavons-MacBook-Air.local>
2020-09-03 11:20:00 -0400

    Fixes registry bugs

--- controllers/registry.js
+++ controllers/registry.js
@@ -9,7 +9,7 @@ function create(req, res){
   req.body.item = req.params.id
   Item.create(req.body)
   .then(item => {
-    res.redirect(`/registry/${req.params.id}`, {user: req.user, title: 'Registry', item} )
+    res.redirect(`/registry` )
   })
 }
 
